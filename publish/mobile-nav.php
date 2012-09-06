1ace2f4dd264ab399a7f6ea223bc2f9af8a4f0ea
middle8media <seth@middle8media.com>
2012-09-06 10:01:52 -0400

    edited the build/project.properties to minify all php files

--- publish/mobile-nav.php
+++ publish/mobile-nav.php
@@ -1,16 +1,1 @@
-<section id="sidebar" role="complementary">
-
-	<nav id="sideMenu" role="navigation">
-	  <ul id="sideMainNav" class="nav-bar">
-      <li><a href="/"><i class="icon-home icon-large"></i> Home</a></li>
-      <li><a href="/web/"><i class="icon-picture icon-large"></i> Web</a></li>
-      <li><a href="/film/"><i class="icon-film icon-large"></i> Film</a></li>
-      <li><a href="/clients/"><i class="icon-trophy icon-large"></i> Clients</a></li>
-      <li><a href="/faq/"><i class="icon-question-sign icon-large"></i> FAQ</a></li>
-      <li><a href="/connect/"><i class="icon-comments icon-large"></i> Connect</a></li>
-	  </ul>
-  </nav>
-
-  <?php include 'project-status.php'; ?>
-
-</section>
+<section id=sidebar role=complementary> <nav id=sideMenu role=navigation> <ul id=sideMainNav class=nav-bar> <li><a href="/"><i class="icon-home icon-large"></i> Home</a></li> <li><a href="/web/"><i class="icon-picture icon-large"></i> Web</a></li> <li><a href="/film/"><i class="icon-film icon-large"></i> Film</a></li> <li><a href="/clients/"><i class="icon-trophy icon-large"></i> Clients</a></li> <li><a href="/faq/"><i class="icon-question-sign icon-large"></i> FAQ</a></li> <li><a href="/connect/"><i class="icon-comments icon-large"></i> Connect</a></li> </ul> </nav> <?php include 'project-status.php'; ?> </section>
