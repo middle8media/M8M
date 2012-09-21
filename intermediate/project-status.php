893c6ab5c481952c7cc6f19a56dd60aaa0afdbd5
middle8media <seth@middle8media.com>
2012-09-21 14:51:39 -0400

    changed project status to  October 2012

--- intermediate/project-status.php
+++ intermediate/project-status.php
@@ -1,5 +1,5 @@
 <div class="panel">
   <h5>Client/Project Status:</h5>
-  <p><em><i class="icon-asterisk icon-large style1"></i> We are accepting projects for September 2012.</em></p>
+  <p><em><i class="icon-asterisk icon-large style1"></i> We are accepting projects for October 2012.</em></p>
   <p><a href="/connect/#contact" class="medium button success">Hire Us</a></p>
 </div>
