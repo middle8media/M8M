83c37e8d5af0b2fb22dec6a4cfd3591048c6d6cc
middle8media <seth@middle8media.com>
2012-11-22 00:42:59 -0500

    zurb foundation update to 3.2.2

--- javascripts/foundation/jquery.offcanvas.js
+++ javascripts/foundation/jquery.offcanvas.js
@@ -1,51 +1,50 @@
-
-jQuery(function() {
+;(function (window, document, $) {
   // Set the negative margin on the top menu for slide-menu pages
-  var jQueryselector1 = jQuery('#topMenu'),
-    events = 'click.fndtn touchstart.fndtn';
-  if (jQueryselector1.length > 0) jQueryselector1.css("margin-top", jQueryselector1.height() * -1);
+  var $selector1 = $('#topMenu'),
+    events = 'click.fndtn';
+  if ($selector1.length > 0) $selector1.css("margin-top", $selector1.height() * -1);
 
   // Watch for clicks to show the sidebar
-  var jQueryselector2 = jQuery('#sidebarButton');
-  if (jQueryselector2.length > 0) {
-    jQuery('#sidebarButton').on(events, function(e) {
+  var $selector2 = $('#sidebarButton');
+  if ($selector2.length > 0) {
+    $('#sidebarButton').on(events, function (e) {
       e.preventDefault();
-      jQuery('body').toggleClass('active');
+      $('body').toggleClass('active');
     });
   }
 
   // Watch for clicks to show the menu for slide-menu pages
-  var jQueryselector3 = jQuery('#menuButton');
-  if (jQueryselector3.length > 0)  {
-    jQuery('#menuButton').on(events, function(e) {
+  var $selector3 = $('#menuButton');
+  if ($selector3.length > 0)  {
+    $('#menuButton').on(events, function (e) {
       e.preventDefault();
-      jQuery('body').toggleClass('active-menu');
+      $('body').toggleClass('active-menu');
     });
   }
 
-  // Adjust sidebars and sizes when resized
-  jQuery(window).resize(function() {
-    jQuery('body').removeClass('active');
-    var jQueryselector4 = jQuery('#topMenu');
-    if (jQueryselector4.length > 0) jQueryselector4.css("margin-top", jQueryselector4.height() * -1);
-  });
+  // // Adjust sidebars and sizes when resized
+  // $(window).resize(function() {
+  //   // if (!navigator.userAgent.match(/Android/i)) $('body').removeClass('active');
+  //   var $selector4 = $('#topMenu');
+  //   if ($selector4.length > 0) $selector4.css("margin-top", $selector4.height() * -1);
+  // });
 
   // Switch panels for the paneled nav on mobile
-  var jQueryselector5 = jQuery('#switchPanels');
-  if (jQueryselector5.length > 0)  {
-    jQuery('#switchPanels dd').on(events, function(e) {
+  var $selector5 = $('#switchPanels');
+  if ($selector5.length > 0)  {
+    $('#switchPanels dd').on(events, function (e) {
       e.preventDefault();
-      var switchToPanel = jQuery(this).children('a').attr('href'),
-          switchToIndex = jQuery(switchToPanel).index();
-      jQuery(this).toggleClass('active').siblings().removeClass('active');
-      jQuery(switchToPanel).parent().css("left", (switchToIndex * (-100) + '%'));
+      var switchToPanel = $(this).children('a').attr('href'),
+          switchToIndex = $(switchToPanel).index();
+      $(this).toggleClass('active').siblings().removeClass('active');
+      $(switchToPanel).parent().css("left", (switchToIndex * (-100) + '%'));
     });
   }
 
-  jQuery('#nav li a').on(events, function(e) {
+  $('#nav li a').on(events, function (e) {
     e.preventDefault();
-    var href = jQuery(this).attr('href'),
-      jQuerytarget = jQuery(href);
-    jQuery('html, body').animate({scrollTop : jQuerytarget.offset().top}, 300);
+    var href = $(this).attr('href'),
+      $target = $(href);
+    $('html, body').animate({scrollTop : $target.offset().top}, 300);
   });
-});
+}(this, document, jQuery));
