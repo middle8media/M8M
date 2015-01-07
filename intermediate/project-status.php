26426aa7928bb1b3b5e0f359594c8f2548f906d0
Seth Hall <seth@ushahidi.com>
2015-01-06 19:40:46 -0500

    V3.0
    The new M8M

--- intermediate/project-status.php
+++
@@ -1,5 +0,0 @@
-<div class="panel">
-  <h5>Client/Project Status:</h5>
-  <p><em><i class="icon-asterisk icon-large style1"></i> We are now accepting projects for April & May of 2013.</em></p>
-  <p><a href="/connect/#contact" class="medium button success">Hire Us</a></p>
-</div>
